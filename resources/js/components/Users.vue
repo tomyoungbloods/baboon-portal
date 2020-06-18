9fcb4c44d902be3becc73c50f7c7f83299a2691c
Tom Youngbloods <tomjongbloets@live.nl>
2020-06-18 13:17:07 +0200

    DB Werkt

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -58,21 +58,78 @@
                     <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">
-                    <p>Modal body text goes here.</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Maak gebruiker aan</button>
-                </div>
+                    <form @submit.prevent="createUser">
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <input v-model="form.name" type="text" name="name" placeholder="Naam"
+                                    class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                                <has-error :form="form" field="name"></has-error>
+                            </div>
+
+                            <div class="form-group">
+                                <input v-model="form.email" type="email" name="email" placeholder="E-mail adres"
+                                    class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                                <has-error :form="form" field="email"></has-error>
+                            </div>
+
+                            <div class="form-group">
+                                <select name="type" v-model="form.type" id="type" class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                                    <option value="">Selecteer een gebruikerstype</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="user">Standaard Gebruiker</option>
+                                    <option value="author">Author</option>
+                                </select>
+                                <has-error :form="form" field="type"></has-error>
+                            </div>
+
+                            <div class="form-group">
+                                <textarea v-model="form.bio" type="text" name="bio" id="bio" placeholder="Korte omschrijving"
+                                    class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }">
+                                </textarea>
+                                <has-error :form="form" field="bio"></has-error>
+                            </div>
+
+                            
+
+                            <div class="form-group">
+                                <input v-model="form.password" type="password" name="password" id="password" placeholder="Wachtwoord"
+                                    class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                                <has-error :form="form" field="password"></has-error>
+                            </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Maak gebruiker aan</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                form: new Form({
+                    name: '',
+                    email: '',
+                    password: '',
+                    type: '',
+                    bio: '',
+                    photo: ''
+                })
+            }
+
+        },
+        methods: {
+            createUser(){
+                this.form.post('api/user')
+            }
+        },
         mounted() {
             console.log('Component mounted.')
         }
