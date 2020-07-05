5d756a8d457946f031273d6ab617f49be0bdbc87
Tom Youngbloods <tomjongbloets@live.nl>
2020-07-05 21:53:08 +0200

    eerste test

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -104,14 +104,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </router-link>
             </li>
             <li class="nav-item">
-              <router-link to="/developer" class="nav-link">
-                <i class="nav-icon fas fas fa-cogs"></i>
-                <p>
-                  Developer
-                </p>
-              </router-link>
-            </li>
-            <li class="nav-item">
               <router-link to="/mijnverzoeken" class="nav-link">
                 <i class="nav-icon fas fas fa-tasks"></i>
                 <p>
