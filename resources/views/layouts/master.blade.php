b2906bdfcc4010760480d2103b9f1423d814b5de
Tom Youngbloods <tomjongbloets@live.nl>
2020-07-09 16:45:35 +0200

    update test

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,7 +10,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-
+  <meta name="theme-color" content="#35555d" />
   <title>Baboon Portal</title>
   <link rel="manifest" href="manifest.json" />
  <link rel="stylesheet" href="/css/app.css">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="manifest" href="manifest.json" />
+        <meta name="theme-color" content="#35555d" />
         <title>Laravel</title>
 
         <!-- Fonts -->
