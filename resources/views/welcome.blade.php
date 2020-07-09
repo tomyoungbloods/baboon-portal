06045d74ac0e4a9c791ee8e6a4253a51a967b5f8
Tom Youngbloods <tomjongbloets@live.nl>
2020-07-09 17:19:54 +0200

    App

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="manifest" href="manifest.json" />
+        <meta name=theme-color content=#50b5f2><meta name=apple-mobile-web-app-status-bar-style content=black-translucent>
+        <link href=./splashscreens/iphone5_splash.png media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel=apple-touch-startup-image><link href=./splashscreens/iphone6_splash.png media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel=apple-touch-startup-image><link href=./splashscreens/iphoneplus_splash.png media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel=apple-touch-startup-image><link href=./splashscreens/iphonex_splash.png media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel=apple-touch-startup-image><link href=./splashscreens/iphonexr_splash.png media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel=apple-touch-startup-image><link href=./splashscreens/iphonexsmax_splash.png media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel=apple-touch-startup-image><link href=./splashscreens/ipad_splash.png media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel=apple-touch-startup-image><link href=./splashscreens/ipadpro1_splash.png media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel=apple-touch-startup-image><link href=./splashscreens/ipadpro3_splash.png media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel=apple-touch-startup-image><link href=./splashscreens/ipadpro2_splash.png media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel=apple-touch-startup-image>
         <meta name="theme-color" content="#35555d" />
-        <title>Laravel</title>
+        <meta name=theme-color content=#50b5f2><meta name=apple-mobile-web-app-status-bar-style content=black-translucent>
+        <title>Baboons</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
