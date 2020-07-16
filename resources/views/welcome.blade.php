d2a4ab2a19ca8b27a5281e13a3ae206d0a3e84f3
Tom Youngbloods <tomjongbloets@live.nl>
2020-07-16 19:02:23 +0200

    Baboon Portal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="manifest" href="./manifest.json" />
-        <title>Baboons</title>
+        <title>Baboon Portal</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
